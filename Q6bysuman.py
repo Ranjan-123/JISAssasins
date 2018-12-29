d9330a18fba31cf50a0a014bca33d19786e993ce
Ranjan-123 <46236103+Ranjan-123@users.noreply.github.com>
2018-12-29 14:47:49 +0530

    Add files via upload

--- Q6bysuman.py
+++ Q6bysuman.py
@@ -1,12 +1,10 @@
-my_str = "Hello this Is an Example With cased letters"
+a=input("Enter the names")
+b=input("Enter the name")
+c=input("Enter the name")
+d=input("Enter the name")
+e=input("Enter the name")
+f=[a,b,c,d,e]
+print(f)
+f.sort()
+print(f)
 
-
-
-words = my_str.split()
-
-words.sort()
-
-
-print("The sorted words are:")
-for word in words:
-   print(word)
